550a6f4e4235903bf80e491c80f85fb4f98aaa2e
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-12 19:44:15 +0100

    Initial Commit

---
+++ House Rotation/House_RotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  House_RotationApp.swift
+//  House Rotation
+//
+//  Created by  Ixart on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct House_RotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
